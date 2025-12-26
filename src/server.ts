fbdc8bb8a593f6528fef06ca76b9eb68db1e12d2
Ali_Cheema <119163298+alihaidercheemaa@users.noreply.github.com>
2025-12-27 04:45:15 +0500

    Change server listen address to localhost

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { PORT } from "./config/config";
 import { connectDB } from "./database/db";
 connectDB()
   .then(() =>
-    app.listen(Number(PORT), "0.0.0.0", () =>
+    app.listen(Number(PORT), "127.0.0.1", () =>
       console.log(
         `connected to the database successfully ✔️  \n  Listening on port http://localhost:${PORT}`,
       ),
